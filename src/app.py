08898f73baab330362cc628abac1276d60b5b9c3
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-09-29 13:33:15 -0500

    feat(src/app.py): added HTTP response to create_items API route

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 from database import get_db, startup_event
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models import Item
 app = FastAPI()
 app.add_event_handler("startup", startup_event)
-@app.post("/items/")
+@app.post("/items/",  statuscode=201)
 def create_item(item:Item):
     conn = get_db()
     cursor = conn.cursor()
