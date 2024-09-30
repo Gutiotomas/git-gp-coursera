fd74a0c04b0f688dcab57d4e5fd223041d13f474
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-09-29 19:23:53 -0500

    Revert "feat(src/app.py): added HTTP response to create_items API route"
    This reverts commit 08898f73baab330362cc628abac1276d60b5b9c3.

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 from database import get_db, startup_event
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from models import Item
 app = FastAPI()
 app.add_event_handler("startup", startup_event)
-@app.post("/items/",  statuscode=201)
+@app.post("/items/")
 def create_item(item:Item):
     conn = get_db()
     cursor = conn.cursor()
